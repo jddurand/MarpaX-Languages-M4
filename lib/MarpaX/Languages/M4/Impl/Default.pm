6dc9fd3dcf1947b80d27b25426a475dc75260921
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-02-24 15:22:54 +0100

    Impl/Default.pm: fix strange runtime behaviour under perl < 5.18

--- lib/MarpaX/Languages/M4/Impl/Default.pm
+++ lib/MarpaX/Languages/M4/Impl/Default.pm
@@ -2709,11 +2709,25 @@ EVAL_GRAMMAR
 
         $self->_checkIgnored( 'regexp', @ignored );
 
+        # Very strange behaviour under perl < 5.18: the following statement seems to
+        # be opimized and executed only once:
+        # my $index = ( $string =~ $regexp ) ? $-[0] : -1;
+        #
+        # To get it really re-evaluated everytime, I have to pass via
+        # a string containing the code to re-evaluate.
+        #
+        # Please note that doing:
+        # my $match = eval '$string =~ $regexp';
+        # did not get re-evaluated under perl < 5.18 (!?)
+        my $code = '$string =~ $regexp';
+
         if ( Undef->check($replacement) ) {
             #
             # Expands to the index of first match in string
             #
-            my $index = ( $string =~ $regexp ) ? $-[0] : -1;
+            #
+            my $match = eval '$code';
+            my $index = $match ? $-[0] : -1;
             return "$index";
         }
         else {
@@ -2742,7 +2756,8 @@ EVAL_GRAMMAR
                 \%wantedRegexpIndice,
                 \$maxRegexpIndice
             );
-            if ( $string =~ $regexp ) {
+            my $match = eval '$code';
+            if ( $match ) {
                 my @match = ();
                 foreach ( 0 .. $maxRegexpIndice ) {
                     if ( $_ <= $#+ ) {
