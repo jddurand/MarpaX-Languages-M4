19735896b57a4e4d29de75e26a2964b8f9b903ce
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-03-08 13:59:46 +0100

    M4.pm: Moops style

--- lib/MarpaX/Languages/M4.pm
+++ lib/MarpaX/Languages/M4.pm
@@ -1,34 +1,65 @@
-package MarpaX::Languages::M4;
+use Moops;
+use MarpaX::Languages::M4::Impl::Default;
 
-# VERSION
+# PODCLASSNAME
 
-# AUTHORITY
+# ABSTRACT: M4 pre-processor
 
-# ABSTRACT: M4 implementation in Perl
+class MarpaX::Languages::M4 {
+    extends 'MarpaX::Languages::M4::Impl::Default';
+
+    # VERSION
+
+    # AUTHORITY
+
+    #
+    # We are lazy and just explicitely proxy all Impl methods
+    #
+    method quote           (@args) { $self->impl_quote           (@args) }
+    method unquote         (@args) { $self->impl_unquote         (@args) }
+    method appendValue     (@args) { $self->impl_appendValue     (@args) }
+    method value           (@args) { $self->impl_value           (@args) }
+    method valueRef        (@args) { $self->impl_valueRef        (@args) }
+    method parseIncremental(@args) { $self->impl_parseIncremental(@args) }
+    method parse           (@args) { $self->impl_parse           (@args) }
+    method unparsed        (@args) { $self->impl_unparsed        (@args) }
+    method setEoi          (@args) { $self->impl_setEoi          (@args) }
+    method eoi             (@args) { $self->impl_eoi             (@args) }
+    method raiseException  (@args) { $self->impl_raiseException  (@args) }
+    method line            (@args) { $self->impl_line            (@args) }
+    method debugfile       (@args) { $self->impl_debugfile       (@args) }
+    method rc              (@args) { $self->impl_rc              (@args) }
+    method isImplException (@args) { $self->impl_isImplException (@args) }
+    method macroExecute    (@args) { $self->impl_macroExecute    (@args) }
+    method macroCallId     (@args) { $self->impl_macroCallId     (@args) }
+    method debugfile       (@args) { $self->impl_debugfile       (@args) }
+}
 
 =head1 DESCRIPTION
 
-This package is an implementation of M4, using Marpa::R2 and Moops.
+This package is an implementation of M4.
 
 =head1 SYNOPSIS
 
      use POSIX qw/EXIT_SUCCESS/;
-     use MarpaX::Languages::M4::Impl::Default;
+     use MarpaX::Languages::M4;
      use Log::Any;
      use Log::Any::Adapter;
      use Log::Any::Adapter::Callback;
 
      Log::Any::Adapter->set('Callback', min_level => 'trace', logging_cb => \&_logging_cb);
 
-     my $impl = MarpaX::Languages::M4::Impl::Default->new_with_options();
-     $impl->impl_parse('debugmode(`V\')m4wrap(`test\')');
-     print "Value: " . $impl->impl_value . "\n";
-     print "Rc: " . $impl->impl_rc . "\n";
+     my $m4 = MarpaX::Languages::M4->new_with_options();
+     $m4->parse('debugmode(`V\')m4wrap(`test\')');
+     print "Value: " . $m4->value . "\n";
+     print "Rc: " . $m4->rc . "\n";
 
      sub _logging_cb {
          my ($method, $self, $format, @params) = @_;
          printf STDERR  "$format\n", @args;
      }
+
+=cut
 
 =head1 SEE ALSO
 
