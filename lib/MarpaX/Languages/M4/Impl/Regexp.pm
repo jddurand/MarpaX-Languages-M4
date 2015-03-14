b4ed12f3cce4c81ccbc544821786075afccae387
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-03-14 11:39:47 +0100

    Impl/Regexp.pm: use re::engine::GNU 0.009

--- lib/MarpaX/Languages/M4/Impl/Regexp.pm
+++ lib/MarpaX/Languages/M4/Impl/Regexp.pm
@@ -65,7 +65,7 @@ class MarpaX::Languages::M4::Impl::Regexp {
                 $regexp = qr/$regexpString(?#)/sm;
             }
             else {
-                use re::engine::GNU;
+                use re::engine::GNU 0.009;
                 $regexp = qr/$regexpString/sm;
                 no re::engine::GNU;
             }
@@ -134,7 +134,7 @@ class MarpaX::Languages::M4::Impl::Regexp {
                 }
             }
             else {
-                use re::engine::GNU;
+                use re::engine::GNU 0.009;
                 #
                 # Execute re::engine::GNU engine
                 #
